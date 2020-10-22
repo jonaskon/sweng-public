90500ce7e6c9132300287dcd0a3e30a9baf61a81
Jonas Konrad <jonas.konrad@epfl.ch>
2020-10-22 09:57:18 +0200

    exercices modularity

--- exercises/modularity/modularity-basics/src/main/java/App.java
+++ exercises/modularity/modularity-basics/src/main/java/App.java
@@ -5,38 +5,58 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class App {
+public static class App {
     public static void main(String[] args) throws FileNotFoundException {
-        compute();
+        List<Double> numbers = getNumbers("data");
+        Double mean = computeMean(numbers);
+        Double stdDev = computeStdDev(numbers, mean);
+        List<Double> normalized = normalize(numbers, mean, stdDev);
+        System.out.println(normalized);
+        writeOnFile(normalized, "output");
     }
 
-    public static List<Double> compute() throws FileNotFoundException {
-        File file = new File("data");
-        List<Double> normalized = new ArrayList<>();
+    public static List<Double> getNumbers(String filename) {
+        File file = new File(filename);
         Scanner scanner = new Scanner(file);
         List<Double> numbers = new ArrayList<>();
         while (scanner.hasNextDouble()) {
             double number = scanner.nextDouble();
             numbers.add(number);
         }
+        scanner.close();
+        return numbers;
+    }
+
+    public static Double computeMean(List<Double> numbers) {
         double sum = 0;
         for (double f : numbers) {
             sum += f;
         }
         double mean = sum / numbers.size();
+        return mean;
+    }
+
+    public static double computeStdDev(List<Double> numbers, Double mean) {
         double sumSquare = 0;
         for (double f : numbers) {
             double diff = f - mean;
             sumSquare += diff * diff;
         }
         double std = Math.sqrt(sumSquare / numbers.size());
-        for (double f : numbers) {
-            normalized.add((f - mean) / std);
-        }
-        System.out.println(normalized);
+        return std;
+    }
 
+    public static List<Double> normalize(List<Double> numbers, Double mean, Double stdDev) {
+        List<Double> normalized = new ArrayList<>();
+        for (double f : numbers) {
+            normalized.add((f - mean) / stdDev);
+        }
+        return normalized;
+    }
+
+    public static void writeOnFile(List<Double> normalized, String filename) throws FileNotFoundException {
         try {
-            FileWriter fw = new FileWriter("output");
+            FileWriter fw = new FileWriter(filename);
             for (double n : normalized) {
                 fw.write(Double.toString(n));
                 fw.write("\n");
@@ -46,7 +66,5 @@ public class App {
             System.out.println("Error writing output file");
         }
         System.out.println("Wrote output file.");
-        scanner.close();
-        return normalized;
     }
 }
